7077c1a03944091c602ea5bc7cb292c7b230ec92
Arseniy <fedorik.murs@mail.ru>
2026-05-13 21:17:49 +0500

    Исправления бака с импортом db_session

--- data/__init__.py
+++ data/__init__.py
@@ -2,4 +2,4 @@ from .lots import Lots
 from .lots_api import LotsResource, LotsListResource
 from .users_api import UsersListResource, UsersResource
 from .users import User
-import db_session
+from .db_session import *
